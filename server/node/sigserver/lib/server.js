0720570c6f0173e7a7380a4241634d7a17db38c8
mscheliga <mario@sourcegarden.de>
2011-01-30 02:22:57 +0100

    oh going to sleep now

---
+++ server/node/sigserver/lib/server.js
@@ -0,0 +1,11 @@
+var connect = require('connect');
+
+var server = function (specs) {
+   var that = {};
+   specs = specs || {};
+
+
+   return that;
+};
+
+exports.server = module.exports = server();
